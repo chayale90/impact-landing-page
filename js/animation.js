94831815e687d435bf5788107e85c530d5593b52
Chaya Segal <chayale90@gmail.com>
2023-07-27 19:42:58 +0300

    animation

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+// scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.top-animation', { origin: 'top' });
+ScrollReveal().reveal('.bottom-animation', { origin: 'bottom' });
+ScrollReveal().reveal('.left-animation', { origin: 'left' });
+ScrollReveal().reveal('.right-animation', { origin: 'right' });
+
